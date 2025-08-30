5bd52d6157c5dc3b521542b16e4466e0981fc52b
Louie2312 <louielouie457@gmail.com>
2025-08-30 19:33:07 +0800

    isa pa

--- backend/src/app.js
+++ backend/src/app.js
@@ -72,21 +72,6 @@ app.use(cors({
 }));
 
 
-app.use(express.json({ limit: '50mb' })); 
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-
-// Request logging
-app.use((req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
-  next();
-});
-
-app.use('/api', (req, res, next) => {
-  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
-  next();
-});
-
-app.use('/api', createAuditLog);
 
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
