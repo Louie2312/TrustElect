21f4cd28ced0726dce20a1e0a33d6b32c1ab4443
Louie2312 <louielouie457@gmail.com>
2025-09-24 02:54:04 +0800

    fix backend

--- backend/src/routes/adminPermissionRoutes.js
+++ backend/src/routes/adminPermissionRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { verifyToken, isSuperAdmin, isAdmin } = require('../middlewares/authMiddleware');
+const { verifyToken, isSuperAdmin } = require('../middlewares/authMiddleware');
 const { getPermissions, updatePermissions, checkPermissions } = require('../controllers/adminPermissionController');
 
-router.get('/:adminId', verifyToken, isSuperAdmin, isAdmin, getPermissions);
+router.get('/:adminId', verifyToken, isSuperAdmin, getPermissions);
 
-router.put('/:adminId', verifyToken, isSuperAdmin, isAdmin, updatePermissions);
+router.put('/:adminId', verifyToken, isSuperAdmin,  updatePermissions);
 
 router.get('/:adminId/check', verifyToken, checkPermissions);
 
